58b1422d9b4604cbc5d549e7a3c66581e0a45317
Leo <leo0.moreirah@gmail.com>
2023-05-15 19:56:52 -0300

    sets, gets e novas separações

--- controller/EmissorController.java
+++ controller/EmissorController.java
@@ -2,16 +2,24 @@ package controller;
 
 import model.*;
 import java.util.ArrayList;
+import java.util.Set;
+
 import view.*;
 public class EmissorController {
 
-    public ArrayList<EmissorModel> fakerEmissor (ArrayList<EmissorModel> ListaEmissor){
+    public ArrayList<EmissorModel> fakerEmissor(ArrayList<EmissorModel> listaEmissor){
 
         int x = 10;
         for (int i = 1; i < x; i++){
             EmissorModel emissor = new EmissorModel();
-            emissor.set
+            PessoaModel pessoa = new PessoaModel();
+            pessoa.setContatoModel(null);
+            pessoa.setEnderecoModel(null);
+            pessoa.setInscriçãoEstadual(i);
+            emissor.setPessoaModel(pessoa);
 
         }
+
+        return listaEmissor;
     }
 }
